e4bdbd677f8b955bda2148e7367fb235b61f47d4
njf0 <goggled.mapping.0p@icloud.com>
2026-04-22 16:26:28 +0100

    Enhance search results display with improved formatting and separator options

--- wevva/widgets/search_results.py
+++ wevva/widgets/search_results.py
@@ -26,6 +26,10 @@ class SearchResultsList(OptionList):
         width: 100%;
         scrollbar-size-vertical: 1;
     }
+
+    SearchResultsList > .option-list--separator {
+        color: $primary-muted;
+    }
     """
 
     def __init__(self):
@@ -54,11 +58,13 @@ class SearchResultsList(OptionList):
         self.clear_options()
         self._place_cache.clear()
 
-        for place in places:
+        for index, place in enumerate(places):
             option_id = self._build_place_id(place)
             label = self._format_place_label(place)
             self.add_option(Option(label, id=option_id))
             self._place_cache[option_id] = place
+            if index < len(places) - 1:
+                self.add_option(None)
 
         # Highlight first result
         if self.option_count > 0:
@@ -112,11 +118,18 @@ class SearchResultsList(OptionList):
         """Format place as rich text label."""
         name = place.get('name', '')
         country = place.get('country', '')
-        admin = place.get('admin', '').split(';')
+        admin_parts = []
+        for admin in reversed(place.get('admin', '').split(';')):
+            admin = admin.strip()
+            if admin and admin != name:
+                admin_parts.append(admin)
+            if len(admin_parts) == 2:
+                break
+        admin_parts.reverse()
 
-        label = f'[bold]{name}[/]\n[dim italic]'
-        if admin:
-            label += f'{", ".join([a for a in admin if a != name])}[/]\n'
-        label += f'{country}\n'
+        label = f'[bold]{name}[/]\n'
+        if admin_parts:
+            label += f'[dim italic]{", ".join(admin_parts)}[/]\n'
+        label += country
 
         return label
